9ff1b425468635507071b16dfd6f343c2537cdc4
crozzdev <crozzdev95@outlook.com>
2026-02-19 22:54:52 -0500

    finished bnb exercise week4

---
+++ week_4/bnb/frequently_reviewed.sql
@@ -0,0 +1,6 @@
+CREATE VIEW "frequently_reviewed" AS
+SELECT listings."id",listings."property_type",listings."host_name", COUNT(reviews."id") AS "reviews" FROM listings
+JOIN reviews ON reviews."listing_id" = listings."id"
+GROUP BY listings."id"
+ORDER BY "reviews" DESC, listings."property_type" ASC, listings."host_name" ASC
+LIMIT 100;
